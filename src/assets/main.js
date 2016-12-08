1e8735e70cd50f04747caf47a594dca2365baa5d
an.smirnov <sandark7@gmail.com>
2016-12-09 00:41:58 +0300

    Done

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/1627589.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var badges = response.courses.completed;
+      var $badges = $('#badges');
 
-  // your code will go here
+      badges.forEach(function(item) {
+        var $item = $('<div />', { class: 'course' });
+
+        $item.append($('<h3>' + item.title + '</h3>'));
+        $item.append($('<img src="' + item.badge + '"/>'));
+        $item.append($('<a target="_blank" class="btn btn-primary" href="' + item.url + '">See Course</a>'));
+        
+        $badges.append($item);
+      });
+    }
+  });
 
 });
